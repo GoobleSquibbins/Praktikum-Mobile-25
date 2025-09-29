4fccd4fd5356728e787198509aebb1320736e28e
GoobleSquibbins <arsyaarsya2005@gmail.com>
2025-09-29 21:11:53 +0700

    Tugas 2 Pertemuan 3

---
+++ app/src/main/java/com/unsoed/informatikamobile/MainActivity2a.kt
@@ -0,0 +1,21 @@
+package com.unsoed.informatikamobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.prak_9_9.R
+
+class MainActivity2a : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_activity2a)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
